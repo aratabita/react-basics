62d7a35face7b3c94f6b798eda1ab5548bb5bb3e
For Private <>
2020-09-28 16:21:20 +0900

    modified app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
 function App() {
+    const profiles = [
+        {
+            name:"Taro", age:10
+        },
+        {
+            name:"Hanako", age:5
+        },
+        {
+            name:"Noage"
+        },
+    ]
   return (
     <div>
-      <h1>Hello World</h1>
+        {
+            profiles.map((profile,index) =><User name={profile.name} age={profile.age} key={index}/>)
+        }
     </div>
   );
+}
+
+const User = props => <h1>Hi, I am {props.name}, and I'm {props.age} years old.</h1>
+
+User.defaultProps = {
+    age:1,
 }
 
 export default App;
