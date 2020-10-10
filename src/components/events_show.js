e60dc2a2a18856a432c1793eb559a8c121d46393
For Private <>
2020-10-10 17:36:50 +0900

    add invalid mode

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -32,12 +32,12 @@ class EventsShow extends Component{
     }
 
     async onSubmit(values){
-        await this.props.postEvent(values)
+        await this.props.putEvent(values)
         this.props.history.push('/')
     }
 
     render(){
-        const {handleSubmit, pristine, submitting}=this.props
+        const {handleSubmit, pristine, submitting,invalid}=this.props
         return (
             <form onSubmit={handleSubmit(this.onSubmit)}>
                 <div>
@@ -45,7 +45,7 @@ class EventsShow extends Component{
                     <Field label="Body" name="body" type="text" component={this.renderField} />
                 </div>
                 <div>
-                    <input type="submit" value="Submit" disabled={pristine || submitting}/>
+                    <input type="submit" value="Submit" disabled={pristine || submitting ||invalid}/>
                     <Link to="/">Cancel</Link>
                     <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
                 </div>
@@ -63,14 +63,10 @@ const validate = values => {
 }
 
 const mapStateToProps = (state, ownProps) =>{
-    console.log('________________')
-    console.log(state.readEvent)
-    console.log('________________')
-    console.log(ownProps.match.params.id)
     const event = state.readEvent[ownProps.match.params.id]
     return { initialValues:event,event}
 }
-const mapDispatchToProps = ({deleteEvent,getEvent})
+const mapDispatchToProps = ({deleteEvent,getEvent, putEvent})
 
 export default connect(mapStateToProps, mapDispatchToProps)(
     reduxForm({validate,form:'eventShowForm',enableReinitialize:true})(EventsShow)
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,12 +1,12 @@
 import _ from 'lodash'
-import {READ_EVENT, READ_EVENTS,DELETE_EVENT} from '../actions'
+import {READ_EVENT, READ_EVENTS,DELETE_EVENT,UPDATE_EVENT,CREATE_EVENT} from '../actions'
 
 export default (events = {},action) => {
     switch (action.type){
+        case CREATE_EVENT:
+        case UPDATE_EVENT:
         case READ_EVENT:
             const {data} = action.response
-            console.log('____action____')
-            console.log(action.response.data)
            return {...events,[data.id]:data}
         case READ_EVENTS:
             return _.mapKeys(action.response.data,'id')
