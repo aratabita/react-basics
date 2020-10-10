f17559d3ed818e57b82ce7a3fd9d9cb152eafb7a
For Private <>
2020-10-10 17:16:19 +0900

    readevent

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,10 +1,18 @@
 import _ from 'lodash'
-import {READ_EVENTS} from '../actions'
+import {READ_EVENT, READ_EVENTS,DELETE_EVENT} from '../actions'
 
 export default (events = {},action) => {
     switch (action.type){
+        case READ_EVENT:
+            const {data} = action.response
+            console.log('____action____')
+            console.log(action.response.data)
+           return {...events,[data.id]:data}
         case READ_EVENTS:
             return _.mapKeys(action.response.data,'id')
+        case DELETE_EVENT:
+            delete events[action.id]
+            return {...events}
         default:
             return events
     }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import {combineReducers} from 'redux'
 import readEvent from './events'
-
+import {reducer as form} from'redux-form'
 console.log(readEvent)
 
-export default combineReducers({readEvent})
+export default combineReducers({readEvent,form})
 // export default combineReducers({ foo, bar, baz})
